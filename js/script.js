0945d66105a3d6c20c2af18222780d32e79ba8f7
Angus Macrae <angusdmacrae@gmail.com>
2020-05-11 16:47:08 +0800

    HTML and basic styling for login screen

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.getElementById("show-sign-up-btn").addEventListener("click", function () {
+    
+//    document.getElementById("sign-up-form").classList.toggle("visible");
+//    document.getElementById("login-form").classList.toggle("visible");
+    document.getElementById("sign-up-form").classList.toggle("invisible");
+    document.getElementById("login-form").classList.toggle("invisible");
+    
+});
+
+document.getElementById("show-login-btn").addEventListener("click", function () {
+    
+//    document.getElementById("sign-up-form").classList.toggle("visible");
+//    document.getElementById("login-form").classList.toggle("visible");
+    document.getElementById("sign-up-form").classList.toggle("invisible");
+    document.getElementById("login-form").classList.toggle("invisible");
+    
+});
